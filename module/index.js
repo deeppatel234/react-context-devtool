b08ac477e75c63ba70d0d9082697e206b0038b92
Deep Patel <deeppatel234@gmail.com>
2020-12-27 12:05:41 +0530

    improve module

--- module/index.js
+++ module/index.js
@@ -14,35 +14,41 @@ export const ContextDevTool = ({ id, context: Context, displayName }) => {
 
   return (
     <Context.Consumer>
-      {values => {
-        if (typeof window !== 'undefined' && window._REACT_CONTEXT_DEVTOOL) {
+      {(values) => {
+        if (typeof window !== "undefined" && window._REACT_CONTEXT_DEVTOOL) {
           window._REACT_CONTEXT_DEVTOOL({ id, displayName, values });
         }
         return null;
       }}
     </Context.Consumer>
   );
-}
+};
 
 export const debugContextDevtool = (container, options) => {
   debugOptions = { ...debugOptions, ...options };
 
   if (
-      debugOptions.disable ||
-      debugOptions.disableAutoMode ||
-      typeof window === 'undefined' ||
-      (!debugOptions.debugReducer && !debugOptions.debugContext)
+    debugOptions.disable ||
+    debugOptions.disableAutoMode ||
+    typeof window === "undefined" ||
+    (!debugOptions.debugReducer && !debugOptions.debugContext)
   ) {
-      return false;
+    return false;
   }
 
   const hook = window.__REACT_CONTEXT_DEVTOOL_GLOBAL_HOOK;
 
-  if (debugOptions.debugReducer) {
-    hook.helpers.loadHookHelper().then(() => {
+  if (hook && hook.debugFiber) {
+    if (
+      hook.helpers &&
+      hook.helpers.loadHookHelper &&
+      debugOptions.debugReducer
+    ) {
+      hook.helpers.loadHookHelper().then(() => {
+        hook.debugFiber(container, options);
+      });
+    } else {
       hook.debugFiber(container, options);
-    });
-  } else {
-    hook.debugFiber(container, options);
+    }
   }
 };
