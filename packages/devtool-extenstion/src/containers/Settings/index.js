8916027ec481e3e825d55136e8285ccb00e03d3c
Deep Patel <deeppatel234@gmail.com>
2021-04-10 19:37:40 +0530

    improve settings

--- packages/devtool-extenstion/src/containers/Settings/index.js
+++ packages/devtool-extenstion/src/containers/Settings/index.js
@@ -6,33 +6,52 @@ import "./index.scss";
 
 const Settings = () => {
   const [startDebugWhen, setStartDebugWhen] = useState("extensionLoad");
+  const [debugUseReducer, setDebugUseReducer] = useState(true);
+  const [debugContext, setDebugContext] = useState(true);
 
   const onChangeStartDebugWhen = (event) => {
     setStartDebugWhen(event.target.value);
   };
 
+  const onChangeDebugUseReducer = (event) => {
+    setDebugUseReducer(event.target.checked);
+  };
+
+  const onChangeDebugContext = (event) => {
+    setDebugContext(event.target.checked);
+  };
+
   useEffect(() => {
-    chrome.storage.local.get(['startDebugWhen'], function(result) {
-      if (result.startDebugWhen) {
-        setStartDebugWhen(result.startDebugWhen);
+    chrome.storage.local.get(
+      ["startDebugWhen", "debugUseReducer", "debugContext"],
+      function (result) {
+        if (result.startDebugWhen) {
+          setStartDebugWhen(result.startDebugWhen);
+        }
+        if (typeof result.debugUseReducer !== "undefined") {
+          setDebugUseReducer(result.debugUseReducer);
+        }
+        if (typeof result.debugContext !== "undefined") {
+          setDebugContext(result.debugContext);
+        }
       }
-    });
+    );
   }, []);
 
   const onClickSave = () => {
     const settingstoSave = {
       startDebugWhen,
+      debugUseReducer,
+      debugContext,
     };
 
-    chrome.storage.local.set(settingstoSave, function(event) {
-      console.log('Value is set to ', event);
-    });
+    chrome.storage.local.set(settingstoSave);
   };
 
   return (
     <div className="settings-wrapper">
       <div className="setting-section">
-        <div className="setting-section-label">Start debugging</div>
+        <div className="setting-section-label">Start Debugging</div>
         <div className="setting-section-value">
           <div className="setting-radio-wrapper">
             <input
@@ -66,6 +85,35 @@ const Settings = () => {
               <div className="light-text">
                 Start data capturing after page load
               </div>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="setting-section">
+        <div className="setting-section-label">Enable Debug</div>
+        <div className="setting-section-value">
+          <div className="setting-radio-wrapper">
+            <input
+              type="checkbox"
+              id="useReducer"
+              className="setting-radio"
+              checked={debugUseReducer}
+              onChange={onChangeDebugUseReducer}
+            />
+            <label htmlFor="useReducer" className="setting-radio-label">
+              useReducer
+            </label>
+          </div>
+          <div className="setting-radio-wrapper">
+            <input
+              type="checkbox"
+              id="context"
+              className="setting-radio"
+              checked={debugContext}
+              onChange={onChangeDebugContext}
+            />
+            <label htmlFor="context" className="setting-radio-label">
+              Context
             </label>
           </div>
         </div>
